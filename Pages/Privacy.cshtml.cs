594bedcb047cffe564bc5a469207dfeda5dc794f
Benjamin Pašić <bpasic1@etf.unsa.ba>
2025-11-22 12:30:25 +0100

    Initial commit - Add EHS_Benjamin_Pasic .Net project

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EHS_Benjamin_Pasic.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
